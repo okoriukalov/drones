ef807380cf4c894319af194e53ccae182a4c8b14
kariakushscan <47974890+kariakushscan@users.noreply.github.com>
2023-12-18 19:39:07 +0200

    test task solution added

---
+++ mp3.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+simple_goto.py: GUIDED mode "simple goto" example (Copter Only)
+
+Demonstrates how to arm and takeoff in Copter and how to navigate to points using Vehicle.simple_goto.
+
+Full documentation is provided at http://python.dronekit.io/examples/simple_goto.html
+"""
+
+from __future__ import print_function
+import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+
+
+# Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = 'udp:127.0.0.1:1450'
+sitl = None
+
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+
+def condition_yaw(heading, relative=False):
+    """
+    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+    This method sets an absolute heading by default, but you can set the `relative` parameter
+    to `True` to set yaw relative to the current yaw heading.
+
+    By default the yaw of the vehicle will follow the direction of travel. After setting 
+    the yaw using this function there is no way to return to the default yaw "follow direction 
+    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+    For more information see: 
+    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+    """
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        print(" Altitude: ", vehicle.location.global_relative_frame.lat)
+        print(" Altitude: ", vehicle.location.global_relative_frame.lon)
+
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+vehicle.mode = VehicleMode("GUIDED")
+
+
+arm_and_takeoff(100)
+
+print("Set default/target airspeed to 30000")
+vehicle.airspeed = 30000
+
+print("Airspeed=",vehicle.airspeed)
+
+
+print("Going towards zero point for 5 seconds ...")
+point0 = LocationGlobalRelative( 50.450739, 30.461242, 100)
+vehicle.simple_goto(point0)
+
+# sleep so we can see the change in map
+time.sleep(5)
+
+print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+print(" Altitude: ", vehicle.location.global_relative_frame.lat)
+print(" Altitude: ", vehicle.location.global_relative_frame.lon)
+
+vehicle.simple_takeoff(100)
+
+
+print("Going towards first point for 5 seconds ...")
+point1 = LocationGlobalRelative(50.443326, 30.448078, 100)
+vehicle.simple_goto(point1)
+
+
+
+print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+print(" Altitude: ", vehicle.location.global_relative_frame.lat)
+print(" Altitude: ", vehicle.location.global_relative_frame.lon)
+
+
+# sleep so we can see the change in map
+time.sleep(5)
+
+print("Going towards zero point for 5 seconds ...")
+point0 = LocationGlobalRelative( 50.450739, 30.461242, 10)
+vehicle.simple_goto(point0)
+
+# sleep so we can see the change in map
+time.sleep(5) 
+
+condition_yaw(350)
+
+
+print("Returning to Launch")
+vehicle.mode = VehicleMode("RTL")
+
+# Close vehicle object before exiting script
+print("Close vehicle object")
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl:
+    sitl.stop()
